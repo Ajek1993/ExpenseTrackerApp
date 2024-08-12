34c7597fbe7b48bf60014b26932c516ebf62bb91
ajek1993@gmail.com <ajek1993@gmail.com>
2024-08-12 23:17:16 +0200

    add background animation

---
+++ frontend/src/app/(components)/Orb/Orb.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./Orb.module.css";
+
+export default function Orb() {
+  return <div className={styles.orb}>Orb</div>;
+}
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,3 +1,9 @@
+import styles from "./page.module.css";
+
 export default function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <div className={styles.mainLayout}>
+      {/* <div className={styles.innerLayout}></div> */}
+    </div>
+  );
 }
